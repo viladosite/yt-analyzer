4ad7edcf34ce65a449202d52f3a02d2d73d1ad9a
Diogo Fagundes <diogo.fagundes@calindra.com.br>
2022-04-10 18:07:58 -0300

    Pré-content load implementations

--- site/functions.js
+++ site/functions.js
@@ -21,9 +21,68 @@ function changeLang(lang){
 }
 
 function searchChannel(){
-    let channelURL = document.getElementById('channelSelect').value;
-    let channelSplit = channelURL.split("https://www.youtube.com/channel/");
-    let channelName = channelSplit[1];
-    
-    document.getElementById('channelTitle').innerHTML = channelName;
+    var channelURL = document.getElementById('channelSelect').value;
+    if (channelURL == '' || channelURL == null){
+        document.getElementById('tabContent').innerHTML = 'You need to provide a valid channel URL to search';
+    } else {
+        if (channelURL.includes('https://www.youtube.com/channel/')){
+            var channelSplit = channelURL.split("https://www.youtube.com/channel/");
+        }
+        else if (channelURL.includes('https://www.youtube.com/c/')){
+            var channelSplit = channelURL.split("https://www.youtube.com/c/");
+        }
+
+        var channelName = channelSplit[1];
+        var templateAbout = `
+        <div id="overAbout">
+            About ${channelName} channel
+            <br>
+            <br>
+            <br>
+        </div>
+        <div id="overBasicData">
+            Visualizações Totais<br>
+            Inscritos<br>
+            Quantidade de séries/playlists<br>
+            <br>
+            MÉDIAS<br>
+            [MÉDIA] Duração dos vídeos<br>
+            [MÉDIA] Likes dos vídeos<br>
+            [MÉDIA] Comentários nos vídeos<br>
+            <br>
+            CAMPEÕES<br>
+            [TOP] Vídeo mais assistido<br>
+            [TOP] Vídeo mais comentado<br>
+            [TOP] Vídeo mais curtido<br>
+        </div>
+        `;
+
+        document.getElementById('channelTitle').innerHTML = '- ' + channelName;
+        document.getElementById('tabContent').innerHTML = templateAbout;
+    }
+}
+
+
+function searchContent(){
+/*     
+    <div id="overAbout">
+        About the channel
+    </div>
+    <div id="overBasicData">
+        Visualizações Totais<br>
+        Inscritos<br>
+        Quantidade de séries/playlists<br>
+        <br>
+        MÉDIAS<br>
+        [MÉDIA] Duração dos vídeos<br>
+        [MÉDIA] Likes dos vídeos<br>
+        [MÉDIA] Comentários nos vídeos<br>
+        <br>
+        CAMPEÕES<br>
+        [TOP] Vídeo mais assistido<br>
+        [TOP] Vídeo mais comentado<br>
+        [TOP] Vídeo mais curtido<br>
+        
+    </div>
+*/
 }
