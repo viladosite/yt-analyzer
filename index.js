c822df48018d66988e9ee4b73912c473e685aaec
Diogo Fagundes <diogo.fagundes@calindra.com.br>
2022-04-12 11:09:10 -0300

    Creating model routes

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ app.get("/", function(req, res){
 	res.sendFile(__dirname + "/site/index.html");
 });
 
+app.get("/channel", function(req, res){
+	res.sendFile(__dirname + "/data/channel.json");
+});
+
+app.get("/playlist", function(req, res){
+	res.sendFile(__dirname + "/data/playlist.json");
+});
+
+app.get("/video", function(req, res){
+	res.sendFile(__dirname + "/data/video.json");
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 });
 
 // Expõe as pastas publicamente
 app.use(express.static(__dirname + '/site'))
+app.use(express.static(__dirname + '/data'))
