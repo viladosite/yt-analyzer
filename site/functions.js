dcc20c4f60906764e6561e2cfff5512f553ca825
Diogo Fagundes <diogo.fagundes@calindra.com.br>
2022-04-12 16:50:01 -0300

    Separating functions from general js

--- site/functions.js
+++ site/functions.js
@@ -6,81 +6,7 @@ function toggleMenu(menuName){
 }
 
 
-// LANGUAGE OPTIONS
-
-function changeLang(lang){
-    let menu = document.getElementById('langOptions');
-    let langName = document.getElementById('langName');
-    let langFlag = document.getElementById('langFlag');
-
-    menu.classList.toggle("none")
-    langName.textContent = `${lang}`
-    if (lang == 'pt-br'){
-        langFlag.classList.remove('fa-flag-usa')
-        langFlag.classList.add('fa-flag')
-    }
-    if (lang == 'en-us'){
-        langFlag.classList.remove('fa-flag')
-        langFlag.classList.add('fa-flag-usa')
-    }
-}
-
-
-
-
-// GET DATA FUNCTIONS
-function getKey(){
-    var key = document.getElementById('ytKey').value;;
-    return key;
-}
-
-
-function getIdFromURL(url){
-    if (url.includes('https://www.youtube.com/channel/')){
-        var channelId = url.split("https://www.youtube.com/channel/");
-    }
-    else if (url.includes('https://www.youtube.com/c/')){
-        var channelId = url.split("https://www.youtube.com/c/");
-    }
-    return channelId;
-}
-
-
-
-async function getChannelData(userKey, channelId){
-    return fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=id,snippet,brandingSettings,contentDetails,statistics,topicDetails&id=${channelId}&key=${userKey}`).then(result => result.json());
-}
-
-
-
-// MODEL FUNCTIONS
-
-function getChannelModel(){
-    return fetch('/channel').then(result => result.json());
-}
-
-function setChannel(channelObj){
-    var channel = [];
-
-    channel.push({
-        name:channelObj.items[0].snippet.title,
-        description:channelObj.items[0].snippet.description,
-        dateCreated:channelObj.items[0].snippet.publishedAt,
-        totalViews:channelObj.items[0].statistics.viewCount,
-        videosQt:channelObj.items[0].statistics.videoCount,
-        subscriptions:channelObj.items[0].statistics.subscriberCount,
-        links:["string", "string", "string"],
-        avatar:channelObj.items[0].snippet.thumbnails.default.url,
-        averageVideoTime:"int",
-        averageVideoLikes:"int",
-        averageVideoComments:"int"
-    })
-    
-    return channel;
-}
-
-
-
+// SEARCH FUNCTIONS
 async function searchChannel(){
     var channelURL = document.getElementById('channelSelect').value;
     var key = getKey();
@@ -100,11 +26,8 @@ async function searchChannel(){
             <div id="overTabContent">
                 <div id="overTabBasic">
                     <img src="${channel.avatar}">
-                    <p>${channel.name}</p>
-                </div>
-                <div id="overTabDetails">
-                    <h3>Description:</h3>
-                    <p>${channel.description}</p>
+                    <h2>${channel.name}</h2>
+                    <br>
                     <h3>Creation Date:</h3>
                     <p>${channel.dateCreated}</p>
                     <h3>Total views:</h3>
@@ -113,6 +36,10 @@ async function searchChannel(){
                     <p>${channel.videosQt}</p>
                     <h3>Subscriptions:</h3>
                     <p>${channel.subscriptions}</p>
+                </div>
+                <div id="overTabDetails">
+                    <h3>Description:</h3>
+                    <p>${channel.description}</p>
                 </div>
                 <div id="id="overTabNumbers"">
                     <h3>Links:</h3>
@@ -136,8 +63,4 @@ async function searchChannel(){
 
 function searchContent(){
 
-}
-
-function getVideo(id){
-    
 }
